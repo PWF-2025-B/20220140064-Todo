ce1b209df89aedd72328b953d9b46917537070c2
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-03 11:01:54 +0700

    middleware, gate admin

--- 064-todo/routes/web.php
+++ 064-todo/routes/web.php
@@ -23,11 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
     Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
-    Route::get('/user', [UserController::class, 'index'])->name('user.index');
-    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeAdmin'])->name('user.makeadmin');
-    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeAdmin'])->name('user.removeadmin');
-    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-
+    // Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    // Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeAdmin'])->name('user.makeadmin');
+    // Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeAdmin'])->name('user.removeadmin');
+    // Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    
     Route::resource('todo', TodoController::class)->except(['show']);
     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
@@ -35,7 +35,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/todo/{todo}/update', [TodoController::class, 'update'])->name('todo.update');
     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
     Route::delete('/todo', [TodoController::class, 'deldestroyCompleted'])->name('todo.deleteallcompleted');
+});
 
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('user', UserController::class)->except(['show']);
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 });
 
 require __DIR__.'/auth.php';
---
+++ 064-todo/tugas7.md
@@ -0,0 +1,9 @@
+# Tugas 6 
+
+## 1. Middleware & Gate Admin Not Active
+![Alt text](screenshot/tugas7/gate_nonactiveuser.png)
+![Alt text](screenshot/tugas7/gate_nonactiveadmin.png)
+
+## 2. Middleware & Gate Admin Active
+![Alt text](screenshot/tugas7/gate_user.png)
+![Alt text](screenshot/tugas7/gate_admin.png)
