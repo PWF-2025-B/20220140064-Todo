c421db107e61af5ddfecfd809bc5845626ee7ad5
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-24 12:31:16 +0700

    mailtrap

--- 064-todo/routes/web.php
+++ 064-todo/routes/web.php
@@ -15,14 +15,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    // Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
     // Route::get('/user', [UserController::class, 'index'])->name('user.index');
     // Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeAdmin'])->name('user.makeadmin');
@@ -33,7 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
     Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-    Route::patch('/todo/{todo}/update', [TodoController::class, 'update'])->name('todo.update');
+    // Route::patch('/todo/{todo}/update', [TodoController::class, 'update'])->name('todo.update');
     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
     Route::delete('/todo', [TodoController::class, 'deldestroyCompleted'])->name('todo.deleteallcompleted');
 
---
+++ 064-todo/tugas10.md
@@ -0,0 +1,13 @@
+# Tugas 10
+
+## 1. Browser Register
+![Alt text](screenshot/tugas10/register.png)
+
+## 2. Browser Category
+![Alt text](screenshot/tugas10/verif.png)
+
+## 3. Browser Mailtrap
+![Alt text](screenshot/tugas10/mailtrap.png)
+
+## 4. Browser Dashboard
+![Alt text](screenshot/tugas10/dashboard.png)
